000cc4e3c8d1a9c48542a96d977d80d52e7f3803
Kevin Yu <kevinyu567@gmail.com>
2023-06-23 18:59:07 -0700

    Speed up `pos_get` and `dim_get` using regex

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ This is not ready for general use
 
 The input file specified by <PATH> should be a text document containing ONLY Input and Output paths for all imagesets you wish to queue, formatted as below:
 
+```
 InputDir: C:\DummyIn\01_19_23_1\Scan 002\TileScan_001\, OutputDir: C:\DummyIn\01_19_23_1\Scanned\
 InputDir: C:\DummyIn\01_18_23_1b\Scan 002\TileScan_001\, OutputDir: C:\DummyIn\01_18_23_1b\Scanned\
+```
 
 The code expects the TileScan_001 directory to contain all the images from a scan, plus the leicametadata folder with a .xlif inside to position the images
 
@@ -23,7 +25,7 @@ Performance ~1-10s per 20 megapixel file at scale=1, k=4 on the Leica computer, 
 Beyond this you'll need to mess with the epsilon parameter of DBScan.
 * `t_rgb_dist`: Initial pixel thresholding. Pixels have to be within euclidian distance (radius) of RGB value to be included in DB scan
 * `t_hue_dist`: Max distance from target hue in HSV space for a pixel's hue to be considered "good" for DB scan - currently unused
-* `t_color_match_count`: Minimum number of "good" pixels an image must have before we'll bother with DBscan. Otherwise
+* `t_color_match_count`: Minimum number of "good" pixels an image must have before we'll bother with DBscan. Otherwise,
 the image is discarded. This makes for fast filtering of images with no relevant content.
 * `t_min_cluster_pixel_count`: minimum points a DBScan cluster can have to be valid
 * `t_max_cluster_pixel_count`: maximum points a DBScan cluster can have to be valid
--- util/leica.py
+++ util/leica.py
@@ -1,5 +1,15 @@
 import numpy as np
+import re
 import argparse
+
+
+def pos_get(input_dir: str) -> np.ndarray:
+    with open(input_dir + "/leicametadata/TileScan_001.xlif", 'r') as file:
+        rawdata = file.read()
+
+    # Match metadata <Tile> tags in the form of <Tile FieldX="..." FieldY="..." PosX="..." PosY="..." PosZ="..." />
+    matches = re.findall(r"<Tile FieldX=\"(.+)\" FieldY=\"(.+)\" PosX=\"(.+)\" PosY=\"(.+)\" PosZ=\".+\" />", rawdata)
+    return np.array([[int(xd), float(posx), int(yd), float(posy)] for xd, yd, posx, posy in matches])
 
 
 def dim_get(input_dir: str) -> tuple[int, int]:
@@ -8,64 +18,12 @@ def dim_get(input_dir: str) -> tuple[int, int]:
     :param input_dir: The directory containing the microscope file.
     :return: The dimensions of the scan, as a tuple of [x, y].
     """
-    try:
-        with open(input_dir + "/leicametadata/TileScan_001.xlif", 'r') as file:
-            rawdata = file.read()
-        rawdata2 = rawdata.partition('</Attachment>')
-        rawdata = rawdata2[0]
-        size = len(rawdata)
-        xarr = []
-        yarr = []
-        while size > 10:
-            rawdata2 = rawdata.partition('FieldX="')
-            rawdata = rawdata2[2]
-            rawdata3 = rawdata.partition('" FieldY="')
-            xd = int(rawdata3[0])
-            rawdata = rawdata3[2]
-            rawdata4 = rawdata.partition('" PosX="')
-            yd = int(rawdata4[0])
-            rawdata = rawdata4[2]
-            rawdata5 = rawdata.partition('" />')
-            rawdata = rawdata5[2]
-            xarr.append(xd)
-            yarr.append(yd)
-            size = len(rawdata)
-            # print(size)
-        xarr = np.array(xarr)
-        yarr = np.array(yarr)
-        print('Your scan is ' + str(np.max(xarr) + 1) + ' by ' + str(np.max(yarr) + 1))
-        return np.max(xarr) + 1, np.max(yarr) + 1
-    except:
-        return 1, 1
+    data = pos_get(input_dir)
 
+    xmax = int(np.max(data[:, 0]))
+    ymax = int(np.max(data[:, 2]))
 
-def pos_get(input_dir: str):  # finds image location from microscope file
-    filename = input_dir + "/leicametadata/TileScan_001.xlif"
-    print(filename)
-    posarr = []
-    with open(filename, 'r') as file:
-        rawdata = file.read()
-        rawdata2 = rawdata.partition('</Attachment>')
-        rawdata = rawdata2[0]
-        size = len(rawdata)
-        while size > 10:
-            rawdata2 = rawdata.partition('FieldX="')
-            rawdata3 = rawdata2[2].partition('" FieldY="')
-            xd = int(rawdata3[0])
-            rawdata4 = rawdata3[2].partition('" PosX="')
-            yd = int(rawdata4[0])
-            rawdata6 = rawdata4[2].partition('" PosY="')
-            posx = float(rawdata6[0])
-            rawdata8 = rawdata6[2].partition('" PosZ="')
-            posy = float(rawdata8[0])
-            rawdata10 = rawdata8[2].partition('" />')
-            rawdata = rawdata10[2]
-            posarr.append([xd, posx, yd, posy])
-            size = len(rawdata)
-            # print(size)
-        posarr = np.array(posarr)
-        print('Position dict made')
-        return posarr
+    return xmax + 1, ymax + 1
 
 
 if __name__ == '__main__':
