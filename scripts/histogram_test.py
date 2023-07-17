7f790a0058f8391e527ab82000a1a0a996dda3ae
Kevin Yu <kevinyu567@gmail.com>
2023-07-17 10:50:13 -0700

    Quick histogram test to examine color peaks

---
+++ scripts/histogram_test.py
@@ -0,0 +1,93 @@
+import sys
+import time
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+import argparse
+import cv2
+import numpy as np
+from util.processing import bg_to_flake_color, get_avg_rgb
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('tkagg')
+
+k = 4
+
+
+def show_histogram(x, title: str, back: int, flake: int, lim: int = 256):
+    print(f"Plotting {title}")
+
+    plt.plot(x)
+    plt.axvline(back, dashes=(2, 1))
+    plt.axvline(flake, dashes=(2, 1), color='b')
+
+    plt.ylabel('Frequency')
+    plt.xlabel('Intensity')
+    plt.xlim([0, lim])
+    plt.title(title)
+
+    plt.show()
+
+
+def show_img(img, mask=None):
+    reds = cv2.calcHist([img], [0], mask, [256], [0, 256])
+    greens = cv2.calcHist([img], [1], mask, [256], [0, 256])
+    blues = cv2.calcHist([img], [2], mask, [256], [0, 256])
+
+    masked = " (masked)" if mask is not None else ""
+
+    show_histogram(reds, f"Stage {s} -- reds" + masked, back_rgb[0], flake_avg_rgb[0])
+    show_histogram(greens, f"Stage {s} -- greens" + masked, back_rgb[1], flake_avg_rgb[1])
+    show_histogram(blues, f"Stage {s} -- blues" + masked, back_rgb[2], flake_avg_rgb[2])
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    hues = cv2.calcHist([img], [0], mask, [180], [0, 180])
+    saturations = cv2.calcHist([img], [1], mask, [256], [0, 256])
+    values = cv2.calcHist([img], [2], mask, [256], [0, 256])
+
+    show_histogram(hues, f"Stage {s} -- hues" + masked, back_hsv[0], flake_avg_hsv[0], lim=180)
+    show_histogram(saturations, f"Stage {s} -- saturations" + masked, back_hsv[1], flake_avg_hsv[1])
+    show_histogram(values, f"Stage {s} -- values" + masked, back_hsv[2], flake_avg_hsv[2])
+
+
+if __name__ == "__main__":
+    # TODO: new description or abstract
+    parser = argparse.ArgumentParser(
+        description="Find graphene flakes on SiO2. Currently configured only for exfoliator dataset"
+    )
+    parser.add_argument(
+        "--s",
+        required=True,
+        type=int,
+        nargs="+",
+        help="Scan stages to test (ex. --s 246 248 250)"
+    )
+    args = parser.parse_args()
+
+    for s in args.s:
+        img = cv2.imread(f"C:\\04_03_23_EC_1\\Scan 002\\TileScan_001\\TileScan_001--Stage{s}.jpg")
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Calculate background and flake RGB using old method for labelling purposes
+        lowlim = np.array([87, 100, 99])  # defines lower limit for what code can see as background
+        highlim = np.array([114, 118, 114])
+
+        imsmall = cv2.resize(img.copy(), dsize=(256 * k, 171 * k)).reshape(-1, 3)
+        test = np.sign(imsmall - lowlim) + np.sign(highlim - imsmall)
+        pixout = imsmall * np.sign(test + abs(test))
+
+        back_rgb = get_avg_rgb(pixout)
+        back_hsv = cv2.cvtColor(np.uint8([[back_rgb]]), cv2.COLOR_RGB2HSV)[0][0]  # TODO: hacky?
+
+        flake_avg_rgb = bg_to_flake_color(back_rgb)
+        flake_avg_hsv = cv2.cvtColor(np.uint8([[flake_avg_rgb]]), cv2.COLOR_RGB2HSV)[0][0]  # TODO: hacky?
+
+        # Calculate histograms from image
+        show_img(img)
+
+        mask = cv2.inRange(img, lowlim, highlim)
+        mask = cv2.bitwise_not(mask)
+
+        show_img(img, mask)
