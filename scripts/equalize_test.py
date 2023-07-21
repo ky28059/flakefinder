7133f5b44ef74eb673a9cf8b5d362a6c4f876025
Kevin Yu <kevinyu567@gmail.com>
2023-07-21 13:31:47 -0700

    Filter out "too dark" pixels and combine masks

--- scripts/equalize_test.py
+++ scripts/equalize_test.py
@@ -43,6 +43,11 @@ if __name__ == "__main__":
         cv2.imshow(name, img_gray)
         cv2.waitKey()
 
+        # Filter out dark, non-flake chunks that will stay dark after equalization
+        _, dark_mask = cv2.threshold(img_gray, 100, 255, cv2.THRESH_BINARY)
+        cv2.imshow(name, dark_mask)
+        cv2.waitKey()
+
         start = time.time()
         equalized = cv2.equalizeHist(img_gray)
         end = time.time()
@@ -52,13 +57,17 @@ if __name__ == "__main__":
         cv2.imshow(name, equalized)
         cv2.waitKey()
 
-        ret, thresh = cv2.threshold(equalized, 5, 255, cv2.THRESH_BINARY_INV)
-        cv2.imshow(name, thresh)
+        _, equalize_mask = cv2.threshold(equalized, 5, 255, cv2.THRESH_BINARY_INV)
+        cv2.imshow(name, equalize_mask)
+        cv2.waitKey()
+
+        masked = np.bitwise_and(dark_mask, equalize_mask)
+        cv2.imshow(name, masked)
         cv2.waitKey()
 
         # Treat threshold like previous `mask_flake_color()` mask and run rest of algorithm on it
-        masked = apply_morph_open(thresh)
-        # masked = apply_morph_close(masked)
+        masked = apply_morph_open(masked)
+        masked = apply_morph_close(masked)
         cv2.imshow(name, masked)
         cv2.waitKey()
 
