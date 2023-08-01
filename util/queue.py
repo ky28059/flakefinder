ecc99e117bd3530897b4ba32395d9631c50729fa
Kevin Yu <kevinyu567@gmail.com>
2023-08-01 15:20:34 -0700

    Use regex for queue loading

--- util/queue.py
+++ util/queue.py
@@ -3,10 +3,11 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
+import re
 from util.logger import logger
 
 
-def load_queue(input_file_path: str) -> list[list[str]]:
+def load_queue(input_file_path: str) -> list[tuple[str, str]]:
     """
     Loads the queue file at the specified file path.
     :param input_file_path: The path to the queue file.
@@ -15,14 +16,7 @@ def load_queue(input_file_path: str) -> list[list[str]]:
     with open(str(input_file_path)) as file1:
         inputs = file1.readlines()
 
-    config = []
-    for line in inputs:
-        line = line.strip("\n")
-        slicer = line.find("OutputDir:")
-        inputdir = line[10:slicer - 2]  # starts after the length of "InputDir: "
-        slicer2 = slicer + 11
-        outputdir = line[slicer2:]
-        config.append([inputdir, outputdir])
+    config = [re.match(r"InputDir: (.+), OutputDir: (.+)", line).group(1, 2) for line in inputs]
 
     logger.info(f"Loaded config: {config}")
     return config
